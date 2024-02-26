831e9d3da8a6b5bda660a55a2a85a93b601c7f3f
Tobias Agerskov Bay <tobiasabay@gmail.com>
2024-02-26 10:16:15 +0100

    Implement file validation
    Der mangler en test, som jeg mangler hjælp til

--- compiler/main.go
+++ compiler/main.go
@@ -1,7 +1,35 @@
 package main
 
-import . "fmt"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
 
 func main() {
-	Println("Hello World!")
+	filepath := "test.bay" // Change this to the path of the file you want to validate(eg. "test.ygl / bay.test")
+	valid, err := validateFilepath(filepath)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	if valid {
+		fmt.Println("File is valid")
+	} else {
+		fmt.Println("File is not valid")
+	}
+}
+
+// First we check if the path does exist, if not we return an error.
+func validateFilepath(path string) (bool, error) {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		return false, fmt.Errorf("file does not exist: %s", path)
+	}
+
+	//Then we check if the file is a ygl file, if not we return an error.
+	if filepath.Ext(path) != ".ygl" {
+		return false, fmt.Errorf("file is not a go file")
+	}
+	//If the file exists and is a ygl file, we return true and nil.
+	return true, nil
 }
