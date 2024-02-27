6b5dab6392092a7d5ceb3cec4c6d10b56b644d2e
alexrefshauge <arefsh22@student.aau.dk>
2024-02-27 13:51:00 +0100

    help a monkey in need

--- compiler/main.go
+++ compiler/main.go
@@ -1,35 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"os"
-	"path/filepath"
-)
-
 func main() {
-	filepath := "test.bay" // Change to the path of the file you want to validate
-
-	valid, err := validateFilepath(filepath, ".ygl") // Specify the expected extension
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	if valid {
-		fmt.Println("File is valid")
-	} else {
-		fmt.Println("File is not valid")
-	}
-}
-
-func validateFilepath(path string, expectedExtension string) (bool, error) {
-	if _, err := os.Stat(path); os.IsNotExist(err) {
-		return false, fmt.Errorf("file %s does not exist", path)
-	}
-
-	if filepath.Ext(path) != expectedExtension {
-		return false, fmt.Errorf("file has incorrect extension, expected %s", expectedExtension)
-	}
-
-	return true, nil
-}
+	print("Hello World")
 }
