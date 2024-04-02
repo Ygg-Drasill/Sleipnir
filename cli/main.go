cd528785f3fed9301b857e0751c46b69fbdab4c7
Androkless <NO01VU@student.aau.dk>
2024-04-02 13:32:57 +0200

    Giga changes. Different file structure

---
+++ cli/main.go
@@ -0,0 +1,8 @@
+
+package main
+
+import "github.com/Ygg-Drasill/Sleipnir/cli/cmd"
+
+func main() {
+	cmd.Execute()
+}
--- main.go
+++
@@ -1,8 +0,0 @@
-
-package main
-
-import "github.com/Ygg-Drasill/Sleipnir/cmd"
-
-func main() {
-	cmd.Execute()
-}
