9e1af7a9e57a9dacef5d5d771416f89a17f9d0c7
Androkless <113034945+Androkless@users.noreply.github.com>
2024-04-03 11:24:40 +0200

    Update cli/cmd/compile.go
    Co-authored-by: Alexander Refshauge <44145107+alexrefshauge@users.noreply.github.com>

--- cli/cmd/compile.go
+++ cli/cmd/compile.go
@@ -11,12 +11,7 @@ import (
 var compileCmd = &cobra.Command{
 	Use:   "compile",
 	Short: "compiles Ygg-Drasill code",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Long: `Compile YggDrasill code to a webassembly binary.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("compile called")
 	},
