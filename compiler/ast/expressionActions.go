74d47aafd40c8aebfdfad23d2ccf3ab5fa1cc5be
alexrefshauge <arefsh22@student.aau.dk>
2024-04-12 13:35:57 +0200

    refactor action functions

---
+++ compiler/ast/expressionActions.go
@@ -0,0 +1,49 @@
+package ast
+
+func Add(val1, val2 Attribute) (Attribute, error) {
+	intVal1, err := toInt64(val1)
+	if err != nil {
+		return nil, err
+	}
+	intVal2, err := toInt64(val2)
+	if err != nil {
+		return nil, err
+	}
+	return intVal1 + intVal2, nil
+}
+
+func Sub(val1, val2 Attribute) (Attribute, error) {
+	intVal1, err := toInt64(val1)
+	if err != nil {
+		return nil, err
+	}
+	intVal2, err := toInt64(val2)
+	if err != nil {
+		return nil, err
+	}
+	return intVal1 - intVal2, nil
+}
+
+func Mul(val1, val2 Attribute) (Attribute, error) {
+	intVal1, err := toInt64(val1)
+	if err != nil {
+		return nil, err
+	}
+	intVal2, err := toInt64(val2)
+	if err != nil {
+		return nil, err
+	}
+	return intVal1 * intVal2, nil
+}
+
+func Div(val1, val2 Attribute) (Attribute, error) {
+	intVal1, err := toInt64(val1)
+	if err != nil {
+		return nil, err
+	}
+	intVal2, err := toInt64(val2)
+	if err != nil {
+		return nil, err
+	}
+	return intVal1 / intVal2, nil
+}
