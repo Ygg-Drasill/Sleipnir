bdac747d6288dfce267a0b30ed2a95060678d93f
MathiasFM <mathiasmogensen1@gmail.com>
2024-04-21 18:28:42 +0200

    small example of code generation

--- compiler/main.go
+++ compiler/main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"os"
+
 	. "github.com/Ygg-Drasill/Sleipnir/compiler/analysis/lexer"
 	"github.com/Ygg-Drasill/Sleipnir/compiler/ast"
 	"github.com/Ygg-Drasill/Sleipnir/compiler/gocc/parser"
-	"os"
+	"github.com/Ygg-Drasill/Sleipnir/compiler/synthesis"
 )
 
 func main() {
@@ -20,9 +22,21 @@ func main() {
 		fmt.Println(e.Error())
 	} else {
 		fmt.Println(res)
-		bytes, _ := json.MarshalIndent(res, "", "\t")
-		file, _ := os.OpenFile("AST_out.json", os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0666)
-		file.Write(bytes)
-		file.Close()
+		if prog, ok := res.(ast.Program); ok {
+			test := synthesis.GenWrapper(&prog)
+			fmt.Printf("%s", test.String())
+			files, err := os.OpenFile("test.wat", os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0666)
+			if err != nil {
+				panic(err)
+			}
+			files.Write(test.Bytes())
+			files.Close()
+			bytes, _ := json.MarshalIndent(res, "", "\t")
+			file, _ := os.OpenFile("AST_out.json", os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0666)
+			file.Write(bytes)
+			file.Close()
+		} else {
+			fmt.Println("Parse result is not of type ast.Program")
+		}
 	}
 }
--- compiler/synthesis/generator.go
+++ compiler/synthesis/generator.go
@@ -1,1 +1,45 @@
 package synthesis
+
+import (
+	"bytes"
+	"fmt"
+
+	"github.com/Ygg-Drasill/Sleipnir/compiler/ast"
+)
+
+func write(b *bytes.Buffer, code string, args ...interface{}) {
+	b.WriteString(fmt.Sprintf(code, args...))
+}
+
+func GenWrapper(p *ast.Program) bytes.Buffer {
+	var b bytes.Buffer
+	gen(p, &b)
+	return b
+}
+
+func gen(node ast.Attribute, b *bytes.Buffer) string {
+	switch node := node.(type) {
+	case *ast.Program:
+		return genProgram(node, b)
+	case *ast.Node:
+		return genNode(node, b)
+	}
+	return ""
+}
+
+func genProgram(node *ast.Program, b *bytes.Buffer) string {
+
+	write(b, "(module\n")
+
+	for _, nodes := range node.Nodes {
+		gen(nodes, b) // Cast nodes to ast.Program
+	}
+	write(b, ")")
+	return ""
+}
+
+func genNode(node *ast.Node, b *bytes.Buffer) string {
+	value := gen(node.Id, b)
+	write(b, "(func $%s", value)
+	return ""
+}
