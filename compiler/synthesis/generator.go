bebf81a9629fb987062d3009c1d4d4c1b12f1649
alexrefshauge <arefsh22@student.aau.dk>
2024-04-23 20:42:50 +0200

    refactor and start node generation

--- compiler/synthesis/generator.go
+++ compiler/synthesis/generator.go
@@ -7,59 +7,87 @@ import (
 	"github.com/Ygg-Drasill/Sleipnir/compiler/ast"
 )
 
-func write(b *bytes.Buffer, code string, args ...interface{}) {
-	b.WriteString(fmt.Sprintf(code, args...))
+type Generator struct {
+	outBuffer   *bytes.Buffer
+	connections *ast.ConnectionList
 }
 
+func (g *Generator) write(code string, args ...interface{}) {
+	g.outBuffer.WriteString(fmt.Sprintf(code, args...))
+}
+
+// GenWrapper starts the generation process from the root Program node.
 func GenWrapper(p *ast.Program) bytes.Buffer {
 	var b bytes.Buffer
-	gen(p, &b)
+	generator := &Generator{outBuffer: &b, connections: &p.Connections}
+	generator.gen(p)
 	return b
 }
 
-func gen(node ast.Attribute, b *bytes.Buffer) string {
+func (g *Generator) gen(node ast.Attribute) string {
 	switch node := node.(type) {
 	case *ast.Program:
-		return genProgram(node, b)
+		return g.genProgram(node)
 	case *ast.Node:
-		return genNode(node, b)
+		return g.genNode(node)
 	case *ast.DeclarationList:
-		return genDecLst(node, b)
+		return g.genDecLst(node)
 	}
 	return ""
 }
 
-func genProgram(node *ast.Program, b *bytes.Buffer) string {
+func (g *Generator) genProgram(node *ast.Program) string {
 
-	write(b, "(module\n")
+	g.write("(module\n")
+
+	for _, n := range node.Nodes {
+		for _, outDec := range n.OutDeclarations {
+			outId := n.Id
+			outAss := outDec.AssigneeId
+
+			g.write("(global $%s.%s (mut i32) (i32.const 0))\n", outId, outAss)
+		}
+	}
 
 	for _, nodes := range node.Nodes {
 		nodePtr := &nodes
-		gen(nodePtr, b)
+		g.gen(nodePtr)
 	}
-	write(b, ")")
+	g.write(")")
 	return ""
 }
 
-func genNode(node *ast.Node, b *bytes.Buffer) string {
+func (g *Generator) genNode(node *ast.Node) string {
+	g.write("(func $%s\n", node.Id)
 
-	for _, outDec := range node.OutDeclarations {
-		outId := node.Id
-		outAss := outDec.AssigneeId
-
-		write(b, " (global $%s.%s (mut i32) (i32.const 0))\n", outId, outAss)
+	inputs := make([]ast.Connection, 0)
+	for _, conn := range *g.connections {
+		if conn.InId.NodeId != node.Id {
+			continue
+		}
+		inputs = append(inputs, conn)
 	}
 
+	for _, conn := range inputs {
+		g.write("todo %s -> %s\n", conn.OutId.NodeId, conn.InId.NodeId)
+	}
+
+	g.gen(node.ProcStatements)
+
+	connectedNodes := make(map[string]bool)
+	for _, conn := range *g.connections {
+		if conn.OutId.NodeId == node.Id && !connectedNodes[conn.InId.NodeId] {
+			g.write("call $%s\n", conn.InId.NodeId)
+			connectedNodes[conn.InId.NodeId] = true
+		}
+	}
+	g.write(")\n")
 	return ""
 }
 
-func genConn(node *ast.Junction, b *bytes.Buffer) string {
-	return ""
-}
-
-func genDecLst(node *ast.DeclarationList, b *bytes.Buffer) string {
+func (g *Generator) genDecLst(node *ast.DeclarationList) string {
 
 	value := node
-	write(b, " (param $%s i64)", value)
+	g.write(" (param $%s i64)", value)
 	return ""
 }
