9c6a2f5143e1d2bc71e9ac47da3c459f248a1f4d
LeUnioo <mnisse22@student.aau.dk>
2024-05-08 14:08:31 +0200

    First little test for matchNumber

--- pkg/analysis/lexer/stateFunctions_test.go
+++ pkg/analysis/lexer/stateFunctions_test.go
@@ -1,40 +1,29 @@
 package lexer
 
 import (
-	"fmt"
 	"pgregory.net/rapid"
 	"testing"
 )
 
 func TestStateFunction(t *testing.T) {
 
+	// To perform multi tests
 	rapid.Check(t, func(t *rapid.T) {
 
-		//gen := rapid.StringMatching("[ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvxyz,*¨?`´<>|€$£@!(=){}1234567890]+").Draw(t, "gen")
-		gen := rapid.StringMatching("[ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvxyz1234567890]+").Draw(t, "gen")
-		l := NewFromString(gen)
-		matchLetters(l)
-
-		fmt.Printf("%v \n", l.tokenList)
-
-		// matchNonToken returnes: matchCommentSingle, matchCommentMulti and matchAny
-		//matchNonTokenvariable := matchNonToken(l)
+		// Test for letters using matchNumbers
+		genLetter := rapid.StringMatching("[ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvxyz]+").Draw(t, "genLetter")
+		l := NewFromString(genLetter)
+		v := matchNumbers(l)
+		tokenListLength := len(l.tokenList)
+		if v == nil {
+			t.Fatalf("No tokens was made, Tokenlist: %v length: %d ", l.tokenList, tokenListLength)
+		}
 
 	})
 }
 
-//The scanner is created
-//_ = compiler.NewScanner(tokens)
-//fmt.Printf("%s %s \n", tokens, scanner)
-// Asserts for expected and actual token
-
-// Do not know if this is relevant for the test for the lexer
-// Parser
 /*
-	p := parser.NewParser()
-	p.Context = ast.NewParseContext()
-	_, err := p.Parse(scanner)
-	if len(gen) < 1 && err == nil {
-		t.Fatalf("parsed empty token?: %s", gen)
+	for i, token := range l.tokenList {
+		fmt.Printf("index:%d tokenType:%v tokenvalue:%v \n", i, token.Type, token.IDValue())
 	}
 */
