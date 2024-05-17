4d7b7282e9e06697f11663f11b72bd91a6d1f011
alexrefshauge <arefsh22@student.aau.dk>
2024-05-17 15:52:51 +0200

    improve error handling in generator, avoids hanging tests

--- pkg/generator/control.go
+++ pkg/generator/control.go
@@ -2,25 +2,31 @@ package generator
 
 import "github.com/Ygg-Drasill/Sleipnir/pkg/ast"
 
-func (g *Generator) genIfStatement(statement *ast.IfStatement) string {
-	g.genExpr(&statement.Expression)
+func (g *Generator) genIfStatement(statement *ast.IfStatement) error {
+	if err := g.genExpr(&statement.Expression); err != nil {
+		return err
+	}
 	g.write("(if\n")
 
 	g.write("(then\n")
 	for _, stmt := range statement.BodyStatements {
-		g.genStmt(&stmt)
+		if err := g.genStmt(&stmt); err != nil {
+			return err
+		}
 	}
 	g.write(")\n")
 
 	if len(statement.ElseStatements) > 0 {
 		g.write("(else\n")
 		for _, stmt := range statement.ElseStatements {
-			g.genStmt(&stmt)
+			if err := g.genStmt(&stmt); err != nil {
+				return err
+			}
 		}
 		g.write(")\n")
 	}
 
 	g.write(")\n")
 
-	return ""
+	return nil
 }
--- pkg/generator/generator.go
+++ pkg/generator/generator.go
@@ -2,10 +2,10 @@ package generator
 
 import (
 	"bytes"
+	"errors"
 	"fmt"
 	"github.com/Ygg-Drasill/Sleipnir/pkg/ast"
 	"github.com/Ygg-Drasill/Sleipnir/pkg/generator/standardTemplates"
-	"log"
 )
 
 func (g *Generator) write(code string, args ...interface{}) {
@@ -13,12 +13,12 @@ func (g *Generator) write(code string, args ...interface{}) {
 }
 
 // GenWrapper starts the generation process from the root Program node.
-func (g *Generator) GenWrapper() *bytes.Buffer {
-	g.gen(g.syntaxTree)
-	return g.outBuffer
+func (g *Generator) GenWrapper() (*bytes.Buffer, error) {
+	err := g.gen(g.syntaxTree)
+	return g.outBuffer, err
 }
 
-func (g *Generator) gen(node ast.Attribute) string {
+func (g *Generator) gen(node ast.Attribute) error {
 	switch node := node.(type) {
 	case *ast.Program:
 		return g.genProgram(node)
@@ -33,12 +33,12 @@ func (g *Generator) gen(node ast.Attribute) string {
 	case ast.Expression:
 		return g.genExpr(&node)
 	case int64:
-		g.genInt(node)
+		return g.genInt(node)
 	}
-	return ""
+	return nil
 }
 
-func (g *Generator) genProgram(node *ast.Program) string {
+func (g *Generator) genProgram(node *ast.Program) error {
 	rootNodes := make([]ast.Node, 0)
 
 	g.write("(module\n")
@@ -58,7 +58,7 @@ func (g *Generator) genProgram(node *ast.Program) string {
 		if len(n.TemplateId) > 0 {
 			template := standardTemplates.StandardTemplates[n.TemplateId]
 			if template == nil {
-				log.Fatalf("template does not exist for %s\n", n.TemplateId)
+				return errors.New(fmt.Sprintf("template does not exist for %s\n", n.TemplateId))
 			}
 			for _, outId := range template.Outputs {
 				nodeId := n.Id
@@ -70,41 +70,44 @@ func (g *Generator) genProgram(node *ast.Program) string {
 	}
 
 	for _, nodes := range node.Nodes {
-		g.gen(&nodes)
+		if err := g.gen(&nodes); err != nil {
+			return err
+		}
 	}
 
 	g.genRoots(rootNodes)
 	g.write(")")
-	return ""
+	return nil
 }
 
-func (g *Generator) genStmt(node *ast.Statement) string {
+var UnknownStatementError = errors.New("unknown statement")
+
+func (g *Generator) genStmt(node *ast.Statement) error {
 	if ifStatement, ok := (*node).(ast.IfStatement); ok {
-		g.genIfStatement(&ifStatement)
-		return ""
+		return g.genIfStatement(&ifStatement)
 	}
 	if assStmt, ok := (*node).(ast.AssignmentStatement); ok {
-		g.gen(&assStmt)
-		return ""
+		return g.gen(&assStmt)
 	}
 	if decStmt, ok := (*node).(ast.Declaration); ok {
-		g.genDeclaration(&decStmt)
-		return ""
+		return g.genDeclaration(&decStmt)
 	}
 	if isExitStmt(*node) {
 		g.genExitStmt()
-		return ""
+		return nil
 	}
-	return ""
+	return UnknownStatementError
 }
 
-func (g *Generator) genAssignmentStmt(node *ast.AssignmentStatement) string {
+func (g *Generator) genAssignmentStmt(node *ast.AssignmentStatement) error {
 
 	_, isExpression := node.Expression.(ast.Expression)
 	_, isLiteral := node.Expression.(int64)
 
 	if isExpression || isLiteral {
-		g.gen(node.Expression)
+		if err := g.gen(node.Expression); err != nil {
+			return err
+		}
 	}
 
 	if identifier, ok := g.isIdentifier(&node.Expression); ok {
@@ -124,33 +127,37 @@ func (g *Generator) genAssignmentStmt(node *ast.AssignmentStatement) string {
 		identifier = LocalIdentifier{localVar}
 	}
 
-	g.genAssignment(identifier)
+	if err := g.genAssignment(identifier); err != nil {
+		return err
+	}
 
-	return ""
+	return nil
 }
 
-func (g *Generator) genDeclaration(node *ast.Declaration) string {
+func (g *Generator) genDeclaration(node *ast.Declaration) error {
 	if expr, ok := node.Expression.(ast.Expression); ok {
-		g.gen(&expr)
+		if err := g.gen(&expr); err != nil {
+			return err
+		}
 	}
 
 	g.write("local.set $%s\n", node.AssigneeId)
-	return ""
+	return nil
 }
 
-func (g *Generator) genAssignment(identifier Identifier) string {
+func (g *Generator) genAssignment(identifier Identifier) error {
 	if nodeIdentifier, ok := identifier.(NodeInIdentifier); ok {
 		g.write("%s\n", nodeIdentifier.toSetInstruction())
 	}
 	if localIdentifier, ok := identifier.(LocalIdentifier); ok {
 		g.write("%s\n", localIdentifier.toSetInstruction())
 	}
-	return ""
+	return nil
 }
 
-func (g *Generator) genInt(val int64) string {
+func (g *Generator) genInt(val int64) error {
 	g.write("i32.const %d\n", val)
-	return ""
+	return nil
 }
 
 func (g *Generator) genRoots(rootNodes []ast.Node) {
