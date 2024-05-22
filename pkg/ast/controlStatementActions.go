7049aef114d517954b4579af9e6c8326e18c3f6f
alexrefshauge <arefsh22@student.aau.dk>
2024-05-22 22:17:18 +0200

    fix all LR(1) conflicts - eliminated differantiation between logical and arithmetic expressions + removed string from types

--- pkg/ast/controlStatementActions.go
+++ pkg/ast/controlStatementActions.go
@@ -1,12 +1,21 @@
 package ast
 
-func NewIfStatement(condition, bodyBlock, elseBlock Attribute) (IfStatement, error) {
+import (
+	"fmt"
+	"github.com/Ygg-Drasill/Sleipnir/pkg/gocc/token"
+)
+
+func NewIfStatement(condition, bodyBlock, elseBlock Attribute) (*IfStatement, error) {
 	var conditionExpression Expression
 	var bodyStatementList StatementList
 	var elseStatementList StatementList
 
 	conditionExpression = condition.(Expression)
 
+	op := string(conditionExpression.Operator.(*token.Token).Lit)
+	if op == "+" || op == "-" || op == "/" || op == "*" {
+		return nil, fmt.Errorf("if statement must contain condition")
+	}
 	if bodyBlock != nil {
 		bodyStatementList = bodyBlock.(StatementList)
 	}
@@ -14,7 +23,7 @@ func NewIfStatement(condition, bodyBlock, elseBlock Attribute) (IfStatement, err
 		elseStatementList = elseBlock.(StatementList)
 	}
 
-	return IfStatement{
+	return &IfStatement{
 		Expression:     conditionExpression,
 		BodyStatements: bodyStatementList,
 		ElseStatements: elseStatementList,
