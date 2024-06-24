d6d1cd29eeee9d1ed25f3798d1fc006226c456e2
MathiasFM <mathiasmogensen1@gmail.com>
2024-06-24 16:40:50 +0200

    change import name

--- pkg/generator/imports.go
+++ pkg/generator/imports.go
@@ -2,5 +2,5 @@ package generator
 
 var (
 	imports = "(import \"console\" \"log\" (func $log (param i32)))" +
-		"(import \"console\" \"log\" (func $move (param i32)))"
+		"(import \"screeps\" \"move\" (func $move (param i32)))"
 )
