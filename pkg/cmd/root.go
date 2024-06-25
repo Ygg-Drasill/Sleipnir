6e66420aca8d727c0eee02a639b4e3ab1a47903f
MathiasFM <mathiasmogensen1@gmail.com>
2024-06-25 14:36:27 +0200

    extend readme + add version for cli

--- pkg/cmd/root.go
+++ pkg/cmd/root.go
@@ -1,12 +1,16 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
-	"github.com/Ygg-Drasill/Sleipnir/pkg/compiler"
+	"io"
 	"log"
+	"net/http"
 	"os"
 	"path"
+	"runtime/debug"
 
+	"github.com/Ygg-Drasill/Sleipnir/pkg/compiler"
 	"github.com/spf13/cobra"
 )
 
@@ -22,7 +26,15 @@ var rootCmd = &cobra.Command{
 	Long:  `Sleipnir is the preferred compiler by the norse gods`,
 	Run: func(cmd *cobra.Command, args []string) {
 		if versionBool {
-			fmt.Println("Version 0.0.1")
+			currentVersion := getCurrentVersion()
+			latestVersion, err := getLatestVersion()
+			if err != nil {
+				log.Printf("Error fetching latest version: %v", err)
+				fmt.Printf("Current version: %s\n", currentVersion)
+			} else {
+				fmt.Printf("Current version: %s\n", currentVersion)
+				fmt.Printf("Latest version: %s\n", latestVersion)
+			}
 			return
 		}
 
@@ -64,8 +76,43 @@ func Execute() {
 
 func init() {
 	rootCmd.Flags().BoolVarP(&versionBool, "version", "v", false, "shows current version")
-
 	rootCmd.Flags().StringVarP(&compileString, "compile", "c", "", "Compile an ygl file to wasm")
+	rootCmd.Flags().BoolVarP(&debugBool, "debug", "d", false, "Has to be run with compile\nEnables debug mode and outputs a JSON and WAT file.")
+}
 
-	rootCmd.Flags().BoolVarP(&debugBool, "debug", "d", false, "Has to be run with hammer-time\nEnable debug mode.\nWrite JSON and WAT file.")
+func getCurrentVersion() string {
+	info, ok := debug.ReadBuildInfo()
+	if !ok {
+		return "unknown"
+	}
+	return info.Main.Version
+}
+
+func getLatestVersion() (string, error) {
+	const repoURL = "https://api.github.com/repos/Ygg-Drasill/Sleipnir/releases/latest"
+	resp, err := http.Get(repoURL)
+	if err != nil {
+		return "", fmt.Errorf("error fetching latest version: %w", err)
+	}
+
+	defer func(Body io.ReadCloser) {
+		err = Body.Close()
+		if err != nil {
+
+		}
+	}(resp.Body)
+
+	if resp.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("error fetching latest version: received status code %d", resp.StatusCode)
+	}
+
+	var release struct {
+		TagName string `json:"tag_name"`
+	}
+	err = json.NewDecoder(resp.Body).Decode(&release)
+	if err != nil {
+		return "", fmt.Errorf("error parsing latest version response: %w", err)
+	}
+
+	return release.TagName, nil
 }
