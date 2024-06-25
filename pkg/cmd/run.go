07040de5c5d24e170db89b846f578845207803d3
alexrefshauge <arefsh22@student.aau.dk>
2024-06-25 15:12:53 +0200

    exit run command if no path specified

--- pkg/cmd/run.go
+++ pkg/cmd/run.go
@@ -3,12 +3,19 @@ package cmd
 import (
 	"fmt"
 	"github.com/spf13/cobra"
+	"os"
 )
 
 var runCmd = &cobra.Command{
 	Use:   "run",
 	Short: "Run a WebAssembly binary",
+	Long: `Run a WebAssembly binary in a wasmtime instance
+(no screeps nodes will run properly)`,
 	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) < 1 {
+			fmt.Println("You need to specify the binary to run")
+			os.Exit(1)
+		}
 		path := args[0]
 		binary, err := readBinaryFromFile(path)
 		handleError(err)
